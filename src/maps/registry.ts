YourDeveloperFriend/choochoo

Initial Scotland blueprint

src/maps/registry.ts
@@ -1,3 +1,4 @@
+import { ScotlandMapSettings } from "./scotland/settings";
 import { AlabamaRailwaysMapSettings } from "./alabama_railways/settings";
 import { SicilyMapSettings } from "./sicily/settings";
 import { GameKey } from "../api/game_key";
@@ -27,6 +28,7 @@ export class MapRegistry {
   private readonly maps = new Map<GameKey, MapSettings>();
 
   private constructor() {
+    this.add(new ScotlandMapSettings());
     this.add(new AlabamaRailwaysMapSettings());
     this.add(new SicilyMapSettings());
     this.add(new RustBeltMapSettings());

Grimiku/choochoo2

src/maps/registry.ts
@@ -5,6 +5,7 @@ import { MapSettings } from "../engine/game/map_settings";
 import { assert } from "../utils/validate";
 import { AlabamaRailwaysMapSettings } from "./alabama_railways/settings";
 import { CyprusMapSettings } from "./cyprus/settings";
+import { DCMetroMapSettings } from "./d_c_metro/settings";
 import { DetroitBankruptcyMapSettings } from "./detroit/settings";
 import { DiscoInfernoMapSettings } from "./disco/settings";
 import { GermanyMapSettings } from "./germany/settings";
@@ -13,6 +14,7 @@ import { IndiaSteamBrothersMapSettings } from "./india-steam-brothers/settings";
 import { IrelandMapSettings } from "./ireland/settings";
 import { JamaicaMapSettings } from "./jamaica/settings";
 import { KoreaWallaceMapSettings } from "./korea-wallace/settings";
+import { LondonMapSettings } from "./london/settings";
 import { MadagascarMapSettings } from "./madagascar/settings";
 import { MontrealMetroMapSettings } from "./montreal_metro/settings";
 import { MoonMapSettings } from "./moon/settings";
@@ -26,7 +28,6 @@ import { SicilyMapSettings } from "./sicily/settings";
 import { SoulTrainMapSettings } from "./soultrain/settings";
 import { StLuciaMapSettings } from "./st-lucia/settings";
 import { SwedenRecyclingMapSettings } from "./sweden/settings";
-import { LondonMapSettings } from "./london/settings";
 
 export class MapRegistry {
   static readonly singleton = new MapRegistry();