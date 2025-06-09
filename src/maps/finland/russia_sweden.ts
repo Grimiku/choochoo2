005c3f38b54fd4129fab482bc93c09b315e62888
Grimiku <kanciok01@yahoo.pl>
2025-06-09 13:30:17 +0200

    [Finland] Switching to SwedenTemp as Land and seeding it

--- src/maps/finland/russia_sweden.ts
+++ src/maps/finland/russia_sweden.ts
@@ -1,6 +1,6 @@
 import z from "zod";
-import { allGoods  } from "../../engine/state/good";
-import { CityData } from "../../engine/state/space";
+import { allGoods } from "../../engine/state/good";
+import { CityData, LandData } from "../../engine/state/space";
 import { SpaceType } from "../../engine/state/location_type";
 import { MovePhase } from "../../engine/move/phase";
 import { injectCurrentPlayer } from "../../engine/game/state";
@@ -36,23 +36,29 @@ export const SWEDEN: CityData = {
   startingNumCubes: 0,
 };
 
-export const SWEDEN_TEMP: CityData = {
-  type: SpaceType.CITY,
-  color: [],
-  name: "Sweden_Temp",
-  goods: [],
-  onRoll: [],
-  startingNumCubes: 5,
+export const SWEDEN_TEMP: LandData = {
+  type: SpaceType.UNPASSABLE,
 };
 
 const FOUR_LOCO_FLAG = new Key("fourLocoFlag", { parse: z.boolean().parse });
 
 export class FinlandStarter extends GameStarter {
   private readonly fourLoco = injectState(FOUR_LOCO_FLAG);
+  protected readonly ghelper = inject(GoodsHelper);
 
   onStartGame() {
     super.onStartGame();
     this.fourLoco.initState(false);
+    this.placeGoodsOnST();
+  }
+
+  placeGoodsOnST() {
+    const startingSweden = this.ghelper.drawGoods(5);
+    const swedenCoord = Coordinates.from({q: 6, r: 4});
+    const swedenTemp = this.gridHelper.lookup(swedenCoord) as Land;
+    this.gridHelper.update(swedenTemp.coordinates, (landData) => {
+      landData.goods = startingSweden;
+    });
   }
 }
 
@@ -78,7 +84,7 @@ export class FinlandSelectAction extends SelectAction {
 
   protected override applyLocomotive(): void {
     super.applyLocomotive();
-    if (this.currentPlayer().locomotive === 4 && !this.fourLoco()){
+    if (this.currentPlayer().locomotive === 2 && !this.fourLoco()){
       this.fourLoco.set(true);
       unlockSweden(this.grid);
     }
@@ -86,9 +92,8 @@ export class FinlandSelectAction extends SelectAction {
 }
 
 function unlockSweden( grid: GridHelper ): void {
-  const swedenTemp = [...grid.findAllCities()].find(
-      (city) => city.data.name === "Sweden_Temp",
-    )!;
+  const swedenCoord = Coordinates.from({q: 6, r: 4});
+  const swedenTemp = grid.lookup(swedenCoord) as Land;
   const sweden = [...grid.findAllCities()].find(
       (city) => city.data.name === "Sweden",
     )!;
@@ -97,8 +102,8 @@ function unlockSweden( grid: GridHelper ): void {
   grid.update(sweden.coordinates, (cityData) => {
       cityData.goods = initialSwedenGoods;
     });
-  grid.update(swedenTemp.coordinates, (cityData) => {
-    cityData.goods = [];
+  grid.update(swedenTemp.coordinates, (landData) => {
+    landData.goods = [];
   });
 }
 
