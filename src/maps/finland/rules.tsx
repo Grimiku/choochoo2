4d3aa4c8f607210300469f20acdc14838e48c0d2
Grimiku <kanciok01@yahoo.pl>
2025-06-16 11:25:52 +0200

    [Finland] Cube removal rules wording update

--- src/maps/finland/rules.tsx
+++ src/maps/finland/rules.tsx
@@ -10,10 +10,10 @@ export function FinlandRules() {
           <b>Espoo-Helsinki-Vantaa:</b> those three cities are linked internally with public transport connections. Goods can move between them via public transport connections, however using them does not grant any income. 
         </li>
         <li>
-          <b>Russia:</b> accepts all colors of goods for delivery and starts with 5 good cubes. When performing a delivery to Russia a random cube is removed from that hex. If Russia becomes empty, goods can no longer be delivered there. You cannot build a link starting from Russia, only towards it.
+          <b>Russia:</b> accepts all colors of goods for delivery and starts with 5 good cubes. After performing a delivery to Russia the player who made the delivery removes a good cube from Russia hex from the game. If Russia becomes empty, goods can no longer be delivered there. You cannot build a link starting from Russia, only towards it.
         </li>
         <li>
-          <b>Sweden:</b> accepts all colors of goods for delivery. You cannot deliver to Sweden unless one of the players reaches Loco 4. At that moment 5 good cubes visible near Sweden hex are moved onto it. When performing a delivery to Sweden a random cube is removed from that hex. If Sweden becomes empty, goods can no longer be delivered there. You cannot build a link starting from Sweden, only towards it.
+          <b>Sweden:</b> accepts all colors of goods for delivery. You cannot deliver to Sweden unless one of the players reaches Loco 4. At that moment 5 good cubes visible near Sweden hex are moved onto it. After performing a delivery to Sweden the player who made the delivery removes a good cube from Sweden hex from the game. If Sweden becomes empty, goods can no longer be delivered there. You cannot build a link starting from Sweden, only towards it.
         </li>
       </ul>
     </div>
